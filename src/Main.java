1f3a5d7ad8a13f31ba34eba85ad1c89672e88e60
Galkina <darja.galkin@gmail.com>
2025-04-18 16:42:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int initialScore = 200;
+        int replenishment = 1_500;
+        int bonus;
+
+        boolean x = replenishment > 1_000;
+
+        if (x) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Итоговый счет: " + (initialScore + replenishment + bonus));
+        System.out.println("Бонусные рубли: " + bonus);
+    }
+}
